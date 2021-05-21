5904907cca1edc5d9d4ef93e5ee4d762c8613298
rishabhdasu <rishabhdasu@gmail.com>
2021-05-22 02:52:52 +0530

    Functions

--- block-BJaabv/code/index.js
+++ block-BJaabv/code/index.js
@@ -8,8 +8,9 @@
   * [ ] Add an additional argument to the function that takes the conversion rate of human to dog years.
 */
 
-function calculateDogAge() {
-  // Your code goes here
+function calculateDogAge(puppy, conversion) {
+     puppy = +prompt(`Enter your puppy's age`);
+    return `Your dog's age is ${puppy * 7}`
 }
 
 /*
@@ -19,8 +20,13 @@ function calculateDogAge() {
   * [ ] Return the total number of movie you will watch.
 */
 
-function calculateMoviesToWatch() {
-  // Your code goes here
+function calculateMoviesToWatch(age, movieWeek) {
+  age = +prompt(`Enter your age`);
+  movieWeek = +prompt(`Number of movies you watch a week`);
+  let movieMonth = movieWeek * 4;
+  let movieYear = movieMonth * 12;
+  let constAge = 100;
+  return `The total number of movies you will watch in you life is ${movieYear * (constAge - age)}`
 }
 
 /*
@@ -29,8 +35,9 @@ function calculateMoviesToWatch() {
   * [ ] Convert it to fahrenheit and return "NN°F is NN°C"
 */
 
-function celsiusToFahrenheit() {
-  // Your code goes here
+function celsiusToFahrenheit(temp) {
+  temp = +prompt(`Enter temperature in Celcius`);
+  return  `Temperature in fahrenheit is ${(temp*9)/5 + 32}`;
 }
 
 /*
@@ -39,10 +46,11 @@ function celsiusToFahrenheit() {
   * [ ] Convert it to celsius and return "NN°F is NN°C"
 */
 
-function celsiusToFahrenheit() {
-  // Your code goes here
-}
+function fahrenheitToCelsius(temp) {
+  temp = +prompt(`Enter temperature in Fahrenheit`);
+  return  `Temperature in Celsius is ${((temp - 32)*5)/9}`;
 
+}
 /*
 5. 🎖 Write a function pow(n, x) that returns x in power n.
 
@@ -51,8 +59,14 @@ function celsiusToFahrenheit() {
   * [ ] If the value of n is below 0 return "The number below 1 is not allowed"
 */
 
-function pow() {
-  // Your code goes here
+function pow(x, n) {
+  x = +prompt(`Enter a number`);
+  n = +prompt(`Enter power`);
+  if(n < 0){
+    return `The number below 1 is not allowed`;
+  }else {
+    return x ** n;
+  }
 }
 
 // Test
@@ -66,8 +80,26 @@ pow(-31, 2); // "The number below 1 is not allowed"
 and return sum or product of 1,…,n. If user enters any other value than `sum` or `product` alert `Not a valid Input`.
 */
 
-function sumOrProductOfN() {
-  // Your code goes here
+function sumOrProductOfN(n, sumOrProd) {
+  n = +prompt(`Enter a number`);
+  sumOrProd = prompt(`sum or product`);
+  let result = 0;
+ if(sumOrProd !== "sum" && sumOrProd !== "product"){
+   return `Not a valid input`;
+ }else {
+   switch(sumOrProd) {
+     case "sum":
+      for(let i = 1; i <= n; i++){
+        result += i;
+      }
+        break;
+      case "product":
+      for(let i = 1; i <= n; i++){
+        result *= i;   
+      }
+   }
+ }
+ return result;
 }
 
 sumOrProductOfN(4, 'sum'); // 10
@@ -78,22 +110,42 @@ sumOrProductOfN(4, 'hello'); // alert "Not a valid Input"
 6. 🎖Write a program that accepts a number n using argument and return the sum of the numbers 1 to n
 */
 
-function sumOfN() {
-  // Your code goes here
+function sumOfN(n) {
+  n = +prompt(`Enter a number`);
+  for(let i = 1; i <= n; i++){
+    result += i;
+  }
+  return result;
 }
 
 /*
 7. 🎖Modify the previous program such that only multiples of 5 or 7 are considered in the sum, e.g. n = 20 (5,7,10,14,15,20) 71
 */
 
-// Your code goes here
+function sumOfMultiples(n){
+  n = +prompt(`Enter a number`);
+  let result = 0;
+  for(let i = 5; i <= n;i++){
+    while(i % 5 === 0 || i % 7 === 0){
+      result += i;
+      i++
+    }
+  }
+  return result;
+}
 
 /*
 8. 🎖Write a function `min` that takes two arguments and returns their minimum.
 */
 
-function min() {
-  // Your code goes here
+function min(a, b) {
+  a = +prompt(`Enter a number`);
+  b = +prompt(`Enter another number`);
+ if(a > b){
+   return `${b} is minimum`;
+ }else {
+  return `${a} is minimum`;
+ }
 }
 
 min(0, 10);
@@ -103,6 +155,7 @@ min(0, -10);
 9. 🎖Write a JavaScript function which accepts an argument and returns the type of the value.
 */
 
-function typeCheck() {
-  // Your code goes here
+function typeCheck(type) {
+  type = prompt(`Enter a value`);
+  return typeof type;
 }
